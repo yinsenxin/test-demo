dfd5e15cae54a6dbecbea151d668b16a8b0ce39b
ysx <ysx1031846739@163.com>
2020-12-14 17:04:40 +0800

    取消注释

--- src/main/java/com/mybatis/demo/testtt.java
+++ src/main/java/com/mybatis/demo/testtt.java
@@ -19,26 +19,20 @@ import java.util.stream.Stream;
 public class testtt {
     public static void main(String[] args) throws Throwable {
 
-//        int j;
-//        boolean flag;
-//        for (int i = 2; i < 1000; i++) {
-//            flag = false;
-//            for (j = 2; j < i; j++) {
-//                if (i % j == 0) {
-//                    flag = true;
-//                    break;
-//                }
-//            }
-//            if (flag == false) {
-//                System.out.print(i + "    ");
-//            }
-//        }
-
-
-
-
-
-
+        int j;
+        boolean flag;
+        for (int i = 2; i < 1000; i++) {
+            flag = false;
+            for (j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    flag = true;
+                    break;
+                }
+            }
+            if (flag == false) {
+                System.out.print(i + "    ");
+            }
+        }
     }
 
 }
